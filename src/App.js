583bca9e22bbe14542736d62497cb0276a47a971
Jitendra Dewat <algorithmgeekx@gmail.com>
2021-09-08 22:24:02 +0530

    Good book
    This is an app for book that categories in different genres.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles.css";
+import { useState } from "react";
 
-function App() {
+const bookDB = {
+  javascript: [
+    { name: "Eloquent JavaScript", rating: "4/5" },
+    { name: "You Don't Know JS", rating: "3.5/5" }
+  ],
+
+  fiction: [
+    { name: "Shiva Trilogy",rating: "5/5" },
+    { name: "Harry Potter and the Sorcerer's Stone", rating: "4.5/5" }
+  ],
+  business: [
+    {
+      name: "Never Split the Difference",
+      rating: "3.5/5"
+    },
+    {
+      name: "Loonshots",
+      rating: "5/5"
+    }
+  ]
+};
+
+export default function App() {
+  const [selectedGenre, setGenre] = useState("business");
+  function genreClickHandler(genre) {
+    setGenre(genre);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> 📚 goodbooks </h1>
+      <p style={{ fontSize: "smaller" }}>
+        {" "}
+        Checkout my favorite books. Select a genre to get started{" "}
+      </p>
+
+      <div>
+        {Object.keys(bookDB).map((genre) => (
+          <button
+            onClick={() => genreClickHandler(genre)}
+            className="bookGener"
+          >
+            {genre}
+          </button>
+        ))}
+      </div>
+      <hr />
+      <div className="books-container" >
+        <ul className="list" >
+          {bookDB[selectedGenre].map((book) => (
+            <li
+              key={book.name}
+              className="list-item"
+            >
+              {" "}
+              <div style={{ fontSize: "larger" }}> {book.name} </div>
+              <div style={{ fontSize: "smaller" }}> {book.rating} </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
-
-export default App;
