5f6110c09ec6c00ce42d8301fc094bd5f02a778b
Jitendra Dewat <algorithmgeekx@gmail.com>
2021-09-15 11:06:33 +0530

    database: added some more books to json.

--- src/App.js
+++ src/App.js
@@ -5,12 +5,19 @@ import { useState } from "react";
 const bookDB = {
   javascript: [
     { name: "Eloquent JavaScript", rating: "4/5" },
-    { name: "You Don't Know JS", rating: "3.5/5" }
+    { name: "You Don't Know JS", rating: "3.5/5" },
+    { name: "JavaScript: The Good Parts", rating: "4.5/5" },
+    { name: "A Smarter Way to Learn JavaScript", rating:"4.6/5" },
+    { name: "JavaScript for Kids: A Playful Introduction to Programming", rating:"4.5/5"  }
   ],
 
   fiction: [
     { name: "Shiva Trilogy",rating: "5/5" },
-    { name: "Harry Potter and the Sorcerer's Stone", rating: "4.5/5" }
+    { name: "Harry Potter and the Sorcerer's Stone", rating: "4.5/5" },
+    { name: "A Thousand Splendid Suns", rating: "4.4/5" },
+    { name: "The Secret of the Nagas", rating: "4.1/5"  }
+
+    
   ],
   business: [
     {
@@ -20,7 +27,9 @@ const bookDB = {
     {
       name: "Loonshots",
       rating: "5/5"
-    }
+    },
+    {name: "How to win friends and influence people.", rating: "4.5/5"},
+    {name: "The 4-Hour Workweek", rating: "4/5"}
   ]
 };
 
@@ -31,7 +40,7 @@ export default function App() {
   }
   return (
     <div className="App">
-      <h1> 📚 goodbooks </h1>
+      <h1> 📚📚📚 Good-Books </h1>
       <p style={{ fontSize: "smaller" }}>
         {" "}
         Checkout my favorite books. Select a genre to get started{" "}
